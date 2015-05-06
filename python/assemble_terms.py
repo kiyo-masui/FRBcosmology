b3437a31dbb016ec5dcecae8ea6b3724915947b7
Kiyoshi Masui <kiyo@physics.ubc.ca>
2015-05-06 15:11:26 -0700

    Added sensitivity figure.

--- python/assemble_terms.py
+++ python/assemble_terms.py
@@ -72,15 +72,18 @@ def apply_coeffs(mult_ell, chi, delta):
 
     return t1, t2, t3
 
-def plot_spectra(mult_ell, chi, delta, **kwargs):
+def plot_spectra(mult_ell, chi, delta, local_only=False, **kwargs):
     ells = mult_ell.ells
     t1, t2, t3 = apply_coeffs(mult_ell, chi, delta)
     #trans = lambda t: ells**2 * abs(t)
     trans = lambda t: abs(t)
-    plt.loglog(ells, trans(t1 + t2 + t3), '-k', label='total', **kwargs)
-    plt.loglog(ells, trans(t1), '--b', label='local term', **kwargs)
-    plt.loglog(ells, trans(t2), '-.g', label='cross term', **kwargs)
-    plt.loglog(ells, trans(t3), ':r', label='integral term', **kwargs)
+    if local_only:
+        plt.loglog(ells, trans(t1), '--b', label='local term', **kwargs)
+    else:
+        plt.loglog(ells, trans(t1 + t2 + t3), '-k', label='total', **kwargs)
+        plt.loglog(ells, trans(t1), '--b', label='local term', **kwargs)
+        plt.loglog(ells, trans(t2), '-.g', label='cross term', **kwargs)
+        plt.loglog(ells, trans(t3), ':r', label='integral term', **kwargs)
 
 
 def get_ells():
@@ -92,7 +95,7 @@ def get_ells():
     return np.array(ells)
 
 
-def get_mult_ell(ells=None, limber=False):
+def get_mult_ell(ells=None, limber=True):
     if ells is None:
         ells = get_ells()
     CHI_MAX = 3600.
@@ -100,7 +103,7 @@ def get_mult_ell(ells=None, limber=False):
     return mell
 
 
-def my_plots(mult_ell=None, chi=1800):
+def my_plots(mult_ell=None, chi=1000):
     matplotlib.rcParams.update({'font.size': 14,
                                 'text.usetex' : False,
                                 'figure.autolayout': True})
@@ -108,8 +111,9 @@ def my_plots(mult_ell=None, chi=1800):
     if mult_ell is None:
         mult_ell = get_mult_ell()
     CHI = float(chi)
-    DELTAS = [5., 20., 50., 100.]
-    Y_MIN = 4e-7
+    #DELTAS = [5., 10., 20., 50., 100.]
+    DELTAS = []
+    Y_MIN = 1e-7
     Y_MAX = 1e-4
     X_MIN = 9
     X_MAX = 1001
@@ -134,21 +138,52 @@ def my_plots(mult_ell=None, chi=1800):
                    )
         plt.legend(loc="upper right", labelspacing=.1, frameon=False)
 
-    chi_s = np.linspace(100, 3500, 50)
+    
+
+    # Publication plots.
+    
+    # The terms.
+    D1 = 10.
+    D2 = 50.
+
+    plt.figure()
+    plot_spectra(mult_ell, chi, D1, **kwargs)
+    plt.ylim([Y_MIN, Y_MAX])
+    plt.xlim([X_MIN, X_MAX])
+    plt.xlabel(r"$\ell$",
+               fontsize=18,
+               )
+    plt.ylabel(r"$C^{ss}_\ell(\chi,\chi')$",
+               fontsize=18,
+               )
+    plt.legend(loc="upper right", labelspacing=.1, frameon=False)
+    plot_spectra(mult_ell, chi, D2, local_only=True, **kwargs)
+
+    # Mean source density plot
+    N_TOTAL = 10000
+    F_SKY = 0.5
+
+    chi_s = np.linspace(100, 3500, 50.)
     coef_s = np.array([  d_ln_n(chi) + 2./chi for chi in chi_s ])
+
+    n_bar = n_chi(chi_s)
+    norm = integrate.simps(n_bar * chi_s**2, chi_s) * 4 * np.pi * F_SKY
+    norm /= N_TOTAL
+    n_bar /= norm
 
     plt.figure()
     ax1 = plt.subplot(211)
-    d = chi_s**2 * n_chi(chi_s)
-    d /= np.amax(d)
+    d = chi_s**2 * n_bar * 4 * np.pi * F_SKY
+    #d /= np.amax(d)
     ax1.plot(chi_s, d, **kwargs)
-    plt.ylim(0., 1.1)
-    plt.yticks(np.arange(0.1, 1.0,  0.2))
-    plt.ylabel(r"$\chi^2\bar{n}_f$  ($h/\rm{MPc})$",
+    #plt.ylim(0., 1.1)
+    #plt.yticks(np.arange(0.1, 1.0,  0.2))
+    plt.ylabel(r"$4\pi f_{\rm sky} \chi^2\bar{n}_f$ ($h/\rm{MPc})$",
                fontsize=18,
                )
     
     ax2 = plt.subplot(212, sharex=ax1)
+    ax2.axhline(y=0.0, color='k')
     ax2.plot(chi_s, coef_s, **kwargs)
     plt.ylim(-0.01, 0.02)
     plt.yticks(np.arange(-0.005, 0.02,  0.005))
@@ -165,6 +200,64 @@ def my_plots(mult_ell=None, chi=1800):
     xticklabels = ax1.get_xticklabels()
     plt.setp(xticklabels, visible=False)
     plt.subplots_adjust(hspace=0.0001)
+
+
+    # Sensitivity plot.
+    ells = mult_ell.ells
+    ind_100, = np.where(ells == 100)
+
+    delta_chi_bins = 100.
+    chi_bins = np.arange(550, 3500, delta_chi_bins)
+    # XXX
+    #delta_chi_bins = 200.
+    #chi_bins = np.arange(600, 3500, delta_chi_bins)
+    n_bar_bins = interpolate.interp1d(chi_s, n_bar)(chi_bins)
+
+    #delta_l = np.diff(ells)
+    #ell_bins = (ells[:-1] + ells[1:]) / 2.
+    delta_l = np.diff(ells[0::2])
+    ell_bins = ells[1:-1:2]
+
+    noise = 1. / n_bar_bins / chi_bins**2 / delta_chi_bins
+
+    cum_spectrum = 0.
+    normalization = 0.
+    cum_var = 0.
+    #cum_noise = 0.
+    for ii, chi1 in enumerate(chi_bins):
+        for jj, chi2 in enumerate(chi_bins):
+            if ii == jj:
+                continue
+            var = noise[ii]*noise[jj] / (ell_bins*(ell_bins + 1)*delta_l*F_SKY)
+            t1, t2, t3 = apply_coeffs(mult_ell, (chi1 + chi2)/2, chi1 - chi2)
+            spectrum = t1 + t2 + t3
+            # XXX
+            weight = spectrum[ind_100] / (noise[ii]*noise[jj])
+            #weight = np.sign(spectrum[ind_100])
+            cum_spectrum += weight * spectrum
+            cum_var += weight**2 * var
+            #cum_noise += noise[ii]*noise[jj] * abs(weight)
+            normalization += abs(weight)
+    #print cum_var.shape, cum_spectrum.shape, weight.shape, noise.shape,
+    #print normalization.shape, var.shape
+    errors = np.sqrt(cum_var) / normalization
+    spectrum = cum_spectrum / normalization
+    # Effective noise power spectrum.
+    noise_spec = errors * np.sqrt(ell_bins*(ell_bins + 1)*delta_l*F_SKY)
+    
+    plt.figure()
+    plt.loglog(ells, spectrum, 'k')
+    #plt.loglog(ell_bins, noise_spec, 'k--')
+    spectrum_bins = interpolate.interp1d(ells, spectrum)(ell_bins)
+    plt.errorbar(ell_bins, spectrum_bins, errors, color='k', marker='', ls='')
+    plt.ylabel(r"$\bar{C}^{ss}_\ell$")
+    plt.xlabel(r"$\ell$")
+    Y_MIN = 5e-7
+    Y_MAX = 2e-4
+    plt.ylim([Y_MIN, Y_MAX])
+    plt.xlim([X_MIN, X_MAX])
+
+    
     
 
 
